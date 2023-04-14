a56f242ab54b39d7dd1e9d91c0d0fd4c78a496ce
Bulbsum <garcialopezgerardo6612@gmail.com>
2023-04-14 15:35:28 -0600

    feat(style): add new header style

--- src/components/Header/Header.component.tsx
+++ src/components/Header/Header.component.tsx
@@ -1,10 +1,23 @@
-import { FC, ReactElement } from 'react'
+import { FC, ReactElement, useState } from 'react'
 import * as S from './Header.styles'
+import { Burger } from 'components/Burger'
 
 export const Header: FC = (): ReactElement => {
+	const [burger, setBurger] = useState<boolean>(true)
+
+	const onClickBurguer = () => {
+		setBurger((prev) => !prev)
+	}
+
 	return (
 		<S.Header>
-			<S.Logo alt='logo' src='/logo.png' />
+			<S.HeaderLogo />
+			<S.HeaderSearch type='text' placeholder='Search a pokemon' />
+			<Burger burger={burger} onClickBurger={onClickBurguer} />
+			<S.Socials className={burger ? 'noShowSocials' : 'showSocials'}>
+				<S.HeaderSocial src='/github.png' />
+				<S.HeaderSocial src='/twitter.png' />
+			</S.Socials>
 		</S.Header>
 	)
 }
--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -1,13 +1,84 @@
 import styled from 'styled-components'
 
 export const Header = styled.header`
-  display: grid;
-  place-items: center;
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 5rem;
   height: 6rem;
-  background-color: #1e7f9a;
+  background-color: #1E5DBB;
+  gap: 1rem;
+  @media (max-width: 750px) {
+    padding: 0 2rem;
+  }
+  @media (max-width: 370px) {
+    padding: 0 5px;
+  }
 `
 
-export const Logo = styled.img`
-  height: 5rem;
-  user-select: none;
+export const HeaderLogo = styled.div`
+  background-image: url(/logo.png);
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  width: 25rem;
+  height: 100%;
+  @media (max-width: 750px) {
+    background-image: url(/logo-s.png);
+    width: 7rem;
+    height: 4rem;
+  }
+`
+
+export const HeaderSearch = styled.input`
+  width: 25%;
+  min-width: 10%;
+  height: 50%;
+  border: none;
+  border-radius: 1rem;
+  padding: 4px 2rem;
+  color: white;
+  background-color: #87A9DB;
+  @media (max-width: 750px) {
+    width: 70%;
+  }
+`
+
+export const HeaderBurger = styled.div`
+  @media (max-width: 750px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 5rem;
+    height: 50%;
+    align-self: center;
+    background: none;
+    outline: 0;
+    border: 0;
+    cursor: pointer;
+  }
+`
+
+export const BurguerLine = styled.div`
+  width: 100%;
+  height: 6px;
+  background-color: #387EFF;
+`
+
+export const Socials = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+  height: 6rem;
+  @media (max-width: 750px) {
+    padding: 2px;
+    border-radius: 1rem;
+    background-color: #387EFF;
+  }
+`
+
+export const HeaderSocial = styled.img`
+  height: 75%;
+  border-radius: 50%;
 `
